1ae6f7e1fa1aa6c9b87ad3478a0e929c78b45ee0
kuantanu <119008529+kuantanu@users.noreply.github.com>
2023-08-20 16:54:43 +0200

    Goal

---
+++ lib/features/goal_app/blocs/bloc.dart
@@ -0,0 +1,3 @@
+export './goal_home_bloc/goal_home_bloc.dart';
+export './goal_home_bloc/goal_home_event.dart';
+export './goal_home_bloc/goal_home_state.dart';
---
+++ lib/features/goal_app/blocs/goal_home_bloc/goal_home_state.dart
@@ -0,0 +1,34 @@
+import '../../models/model.dart';
+
+abstract class GoalState {}
+
+class GoalLoading extends GoalState {}
+
+class GoalHasEnoughReflections extends GoalState {
+  final List<Goal> generatedGoals;
+  final List<Goal> personalGoals;
+
+  GoalHasEnoughReflections({required this.generatedGoals, required this.personalGoals});
+}
+
+class GoalInsufficientReflections extends GoalState {}
+
+class GoalError extends GoalState {
+  final String errorMessage;
+
+  GoalError({required this.errorMessage});
+}
+
+class GoalCreationAllowed extends GoalState {}
+class GoalCreationDenied extends GoalState {
+  final String reason;
+
+  GoalCreationDenied(this.reason);
+}
+
+class GoalCreatedSuccessfully extends GoalState {
+  final Goal goal;
+
+  GoalCreatedSuccessfully({required this.goal});
+}
+
