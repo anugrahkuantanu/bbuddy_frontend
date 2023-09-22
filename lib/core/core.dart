949b03b34da6d3005988bd02fe83e8e4039fa16d
kuantanu <119008529+kuantanu@users.noreply.github.com>
2023-09-22 15:08:33 +0200

    update

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -2,3 +2,5 @@ export './classes/classes.dart';
 export './helpers/helpers.dart';
 export './utils/utils.dart';
 export './widgets/widget.dart';
+export './services/service.dart';
+import './models/model.dart';
